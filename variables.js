4eaf555312d30d13583816bf069da2c37d352ead
ricardotm <rjaviertorres96@gmail.com>
2021-05-24 15:42:05 -0500

    Commit for the Submit form

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Para declarar una variable, JavaScript tiene una palabra reservada (que hace parte de otras como let y cont)
+
+var //palabra reservada para una variable
+var nombre // nombre de la variable, ademas, es la etiqueta con la que javascript lo guardará en memoria
+var nombre = "Ricardo" //valor de la variable o "inicializado" SIEMPRE DEBE SER UN (=) EN CASO DE VARIABLES
+var nombre = "Ricardo"; //cerramos o terminamos la sentencia con (;) por sintaxis y buenas praticas
+
+//Declarar una variable:
+var edad;
+//Inicializar una variable:
+edad = 30;
+
+//podemor generar una variable de tipo array
+var elementos = ["computadora", "celular", "reloj"]
+
+//Podemos general variables de tipo objeto
+var usuario = {
+    nombre: "Richard",
+    sexo: "M",
+    edad: 25,
+    nacionalidad: "Colombiano"
+}
